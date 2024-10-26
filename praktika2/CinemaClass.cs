c83a0c3518d5d30d6961f7206d76f5e49aa6a599
Mandartin <maratkaumov019@gmail.com>
2024-10-26 14:53:51 +0300

    Add files via upload

---
+++ praktika2/CinemaClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika2
+{
+    public class Cinema
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ praktika2/FilmClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika2
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date_of_premier { get; set; }
+        public string Janr { get; set; }
+        public bool dostup_pushkin { get; set; }
+
+    }
+}
